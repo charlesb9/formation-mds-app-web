88b1fdd0df45e68f2b96556b551698bdf2b00e69
Damien Baehr <damien.baehr@hotmail.com>
2024-05-17 11:35:17 +0200

    👌 IMPROVE:  add functional button

--- app/src/app/components/button.component.ts
+++ app/src/app/components/button.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      button works!
-    </p>
+    <button 
+      class="px-7 rounded-lg text-white"
+      [ngClass]="variantClass"
+      (click)="handleClick()">
+      {{ label }}
+    </button>
   `,
-  styles: ``
+  styles: [`
+    .normal {
+      background-color: #007bff;
+    }
+    .danger {
+      background-color: #dc3545;
+    }
+  `]
 })
 export class ButtonComponent {
+  @Input() label: string = 'Click me';
+  @Input() variant: 'normal' | 'danger' = 'normal';
+  @Output() onClick = new EventEmitter<void>();
 
+  get variantClass(): string {
+    return this.variant;
+  }
+
+  handleClick(): void {
+    this.onClick.emit();
+  }
 }
