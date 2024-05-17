f7f5b94616d6f8d92ca87a810cda2453e661b3c8
Im3vulx <loic.hlppl@gmail.com>
2024-05-17 12:05:17 +0200

    📦 NEW:  task card, list, from and service

---
+++ app/src/app/taches/taskform.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskform',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      taskform works!
+    </p>
+  `,
+  styles: ``
+})
+export class TaskformComponent {
+
+}
---
+++ app/src/app/taches/tasklist.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasklist',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tasklist works!
+    </p>
+  `,
+  styles: ``
+})
+export class TasklistComponent {
+
+}
