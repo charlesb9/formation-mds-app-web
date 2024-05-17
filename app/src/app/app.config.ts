f1bf6fa62727fc7bda153e9bf2c88719dfcbb9e9
Damien Baehr <damien.baehr@hotmail.com>
2024-05-17 16:35:00 +0200

    👌 IMPROVE: Input Euro

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideAnimations} from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations(), provideRouter(routes)],
-  
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes),
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: "fr"},
+  ],
 };
