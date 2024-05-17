684aa30de359398e735b78506cbe2e043f498b62
Valentin <valentin.renaudin@my-digital-school.org>
2024-05-17 14:54:43 +0200

    [ADD] Crud project

--- api/routes/projet.route.js
+++ api/routes/projet.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const  projetController = require("../controllers/projet.controller");
+const  projetController = require("../controllers/project.controller");
 
 router.get("/", projetController.getAllProjects);
 
