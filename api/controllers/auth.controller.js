7c7e4768bc89b988cd01d214c452500c41e4c1c4
Javor Emir <emir.j2002@gmail.com>
2024-05-17 11:44:44 +0200

    [ADD] JWT auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,20 @@
-//auth controller 
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + '/../../.env.local' });
 
 exports.login = (req, res) => {
     const { email, password } = req.body;
-    res.send(({ email, password }));
 
+    // Corrected jwt.sign method with options object
+    const token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.send({ email, password, token });
 }
 
 exports.register = (req, res) => {
     const { email, password } = req.body;
-    res.send( email, password);
+    res.send({ email, password });
 }
 
 module.exports = exports;
