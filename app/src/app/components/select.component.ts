f1e9c1cb8849670686d6867649a8ac1d83814c53
MelinaClouet <melina.clouet@my-digital-school.org>
2024-05-17 14:56:42 +0200

    [UPD] select avec recherche dans la liste

--- app/src/app/components/select.component.ts
+++ app/src/app/components/select.component.ts
@@ -7,25 +7,54 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <div>
-      <input *ngIf="isSearch" type="text" [(ngModel)]="searchTerm" placeholder="Rechercher..." />
-      <select>
-        <option *ngFor="let item of filteredData()" [value]="item">{{ item }}</option>
-      </select>
+    <div class="dropdown">
+      <div class="dropdown-select" (click)="toggleDropdown()">
+        {{ selectedValue || 'Select an option' }}
+      </div>
+      <div class="dropdown-list" *ngIf="dropdownOpen">
+        <input *ngIf="isSearch" type="text" [(ngModel)]="searchTerm" placeholder="Rechercher..." (input)="onSearchChange()" />
+        <div *ngFor="let item of filteredData()" (click)="selectItem(item)" class="dropdown-item">
+          {{ item }}
+        </div>
+      </div>
     </div>
   `,
   styles: [`
-    div {
-      margin: 10px;
+    .dropdown {
+      position: relative;
+      display: inline-block;
+      width: 200px;
     }
-    input {
-      margin-bottom: 10px;
-      padding: 5px;
-      width: calc(100% - 12px);
+    .dropdown-select {
+      padding: 10px;
+      border: 1px solid #ccc;
+      cursor: pointer;
+      background-color: #fff;
     }
-    select {
-      padding: 5px;
-      width: 100%;
+    .dropdown-list {
+      position: absolute;
+      top: 100%;
+      left: 0;
+      right: 0;
+      border: 1px solid #ccc;
+      background-color: #fff;
+      z-index: 1000;
+      max-height: 200px;
+      overflow-y: auto;
+    }
+    .dropdown-list input {
+      width: calc(100% - 20px);
+      padding: 10px;
+      border: none;
+      border-bottom: 1px solid #ccc;
+      box-sizing: border-box;
+    }
+    .dropdown-item {
+      padding: 10px;
+      cursor: pointer;
+    }
+    .dropdown-item:hover {
+      background-color: #f0f0f0;
     }
   `]
 })
@@ -33,6 +62,24 @@ export class SelectComponent {
   @Input() data: string[] = [];
   @Input() isSearch: boolean = false;
   searchTerm: string = '';
+  selectedValue: string | null = null;
+  dropdownOpen: boolean = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+    if (!this.dropdownOpen) {
+      this.searchTerm = '';
+    }
+  }
+
+  selectItem(item: string) {
+    this.selectedValue = item;
+    this.dropdownOpen = false;
+  }
+
+  onSearchChange() {
+    this.filteredData();
+  }
 
   filteredData(): string[] {
     if (!this.searchTerm) {
