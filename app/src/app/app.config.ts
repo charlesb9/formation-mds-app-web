f9e429df297de504a85f74ffb3cb44f225e2cbad
Damien Baehr <damien.baehr@hotmail.com>
2024-05-17 16:46:26 +0200

    🐛 FIX: config euro input

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -3,11 +3,15 @@ import {
   DEFAULT_CURRENCY_CODE,
   LOCALE_ID,
 } from '@angular/core';
+import fr from '@angular/common/locales/fr';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(fr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
