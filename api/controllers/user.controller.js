f8a558dc0e6961b1839c2a549d0654c32d2996e9
Valentin <valentin.renaudin@my-digital-school.org>
2024-05-17 14:29:52 +0200

    [FIX] Class obsolet

--- api/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-// const db = require('../config/database');
-const User = require('../class/user.class');
-
-exports.getAllUsers= (req, res) => {
-let user = User.fromMap(req.body);
-const requete = ""
-}
-
