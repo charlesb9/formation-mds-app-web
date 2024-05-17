534c35d19478d2e239b234a06d3ee01927715f19
Damien Baehr <damien.baehr@hotmail.com>
2024-05-17 14:35:55 +0200

    init select branch

---
+++ app/src/app/components/select.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      select works!
+    </p>
+  `,
+  styles: ``
+})
+export class SelectComponent {
+
+}
