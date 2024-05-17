889e9a43c1da6a37ffc1720a218be3a058cf8b2d
Javor Emir <emir.j2002@gmail.com>
2024-05-17 15:16:58 +0200

    [ADD] user route, controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 
-// router.get("/", userController.getAllUsers);
-
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
