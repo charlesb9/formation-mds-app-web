79fee069ae5c1454f676f14f352d147e2b2cf230
Javor Emir <emir.j2002@gmail.com>
2024-05-17 11:29:05 +0200

    init auth

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,3 +14,5 @@ app.listen(port, () => {
 })
 
 app.use('/user', require('./routes/user.route'));
+
+app.use('/auth', require('./routes/auth.route'));
