94bd3e72d52383c117878d1db6f94a91540b1a09
Valentin <valentin.renaudin@my-digital-school.org>
2024-05-17 14:27:41 +0200

    [ADD] Debut controller user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,9 @@
 const express = require("express");
+// const db = require('../config/database');
+const User = require('../class/user.class');
 
 exports.getAllUsers= (req, res) => {
-
+let user = User.fromMap(req.body);
+const requete = ""
 }
 
