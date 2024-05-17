92cba76917dca61535baef7b5a8afef7b3f241a4
Javor Emir <emir.j2002@gmail.com>
2024-05-17 14:29:59 +0200

    temp fix

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 
-router.get("/", userController.getAllUsers);
+// router.get("/", userController.getAllUsers);
 
 
 module.exports = router;
